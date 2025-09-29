d13ccdf08bdf18c1707ad93e94f26893642fceb7
Grealishgit <eugyneehunter@gmail.com>
2025-09-29 03:50:26 +0300

    Update user creation button label and change users table header background color for improved UI

--- admin/users.php
+++ admin/users.php
@@ -271,7 +271,7 @@ $users = $authHandler->getAllUsers();
                 </div>
                 <div class="modal-actions">
                     <button type="button" class="btn btn-secondary" onclick="closeUserModal()">Cancel</button>
-                    <button type="submit" class="btn btn-primary">Create User</button>
+                    <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </form>
         </div>
